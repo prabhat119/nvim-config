b1399854ad4826546076c97aa5192010433dee8e
prabhat119 <prabhat119.dev@gmail.com>
2026-04-27 11:43:21 +0530

    updated undotree

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -23,7 +23,6 @@ opt.showbreak = "↪ "
 
 opt.swapfile = false
 opt.backup = false
-opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 opt.undofile = true
 
 opt.hlsearch = false
--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -4,4 +4,16 @@ return {
 	keys = {
 		{ "<leader>u", vim.cmd.UndotreeToggle, desc = "Toggle to see undotree" },
 	},
+	config = function()
+		vim.g.undotree_WindowLayout = 1
+		vim.g.undotree_SetFocusWhenToggle = 1
+
+		local undo_path = vim.fn.expand("~/tools/neovim/undo-tree")
+		if vim.fn.isdirectory(undo_path) == 0 then
+			vim.fn.mkdir(undo_path, "p")
+		end
+
+		vim.opt.undodir = undo_path
+		vim.opt.undofile = true
+	end,
 }
